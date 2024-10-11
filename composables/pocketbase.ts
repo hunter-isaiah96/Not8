bf2258686de555226206b43de6c89b4da95d0e80
Isaiah Hunter <hunter.isaiah96@gmail.com>
2024-10-11 11:21:51 -0400

    init

--- composables/pocketbase.ts
+++
@@ -1,7 +0,0 @@
-import PocketBase from "pocketbase"
-
-const runtimeConfig = useRuntimeConfig()
-const pb = new PocketBase(runtimeConfig.public.pocketbaseURL)
-
-export const usePocketbase = () => pb
-export const isAuthenticated = () => pb.authStore.isValid
